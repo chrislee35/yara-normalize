a90c3783286da00eb56fbfdeffd14ee3a8599036
chrislee35 <rubygems@chrislee.dhs.org>
2012-10-29 17:41:16 -0400

    Version bump to 0.1.0

--- lib/yara-normalize/yara-normalize.rb
+++ lib/yara-normalize/yara-normalize.rb
@@ -1,83 +1,107 @@
-require 'digest/sha1'
-
-module Yara
-	class Rule < Struct.new(:name, :tags, :meta, :strings, :condition)
-		def hash_code
-			normalized_strings = strings.map{|x| x.gsub(/^\s*\$\w+\s*=\s*/,'')}.sort.join("%")
-			strings_hash = Digest::SHA1.hexdigest(normalized_strings)
-			condition_hash = Digest::SHA1.hexdigest(normalized_condition)
-			#pp normalized_strings
-			#pp normalized_condition
-			"#{strings_hash}#{condition_hash}"
-		end
-		
-		def condition_var_replace(condition)
-			vars = {}
-			nextvar = 'a'
-			condition.gsub(/\$\w+/) do |x|
-				unless vars[x]
-					vars[x] = "\$#{nextvar}"
-					nextvar = (nextvar[0] + 1).chr
-				end
-				vars[x]
-			end
-		end
-		
-		# ($a and $b) or ($c and $d and ($e or $f)) => (($e or $f) and $c and $d) or ($a and $b)
-		# [['$a','and','$b'],'or',['$c','and','$d','and',['$e','or','$f']]]
-		def normalized_condition
-			return condition if condition =~ /(any of them|all of them|any \d+ of them)/i
-			condition_var_replace(condition_rearrange(condition_var_replace(self.condition)).join(","))
-		end
-		
-		def condition_rearrange(condition)
-			c = condition.gsub(/\(/,'[').gsub(/\)/,'],').gsub(/((\$\w+|and|or|not))/) do |x| "'#{x}',"; end.gsub(/,\]/,']').gsub(/,\s*$/,'')
-			arr = eval("[#{c}]")
-			condition_rearrange2(arr)
-		end
-		
-		def condition_rearrange2(subpart)
-			if subpart.is_a? Array
-				subpart.sort {|a,b| 
-					if a.is_a? Array and b.is_a? Array
-						b.flatten.length <=> a.flatten.length
-					elsif a.is_a? Array
-						-1
-					elsif b.is_a? Array
-						1
-					else
-						b.length <=> a.length
+require 'digest/md5'
+require 'pp'
+module YaraTools
+	VERSION = "01"
+	class YaraRule
+		attr_reader :original, :name, :tags, :meta, :strings, :condition, :normalized_strings
+		def initialize(ruletext)
+			ruletext = ruletext.gsub(/[\r\n]+/,"\n").gsub(/^\s*\/\/.*$/,'')
+			@original = ruletext
+			@lookup_table = {}
+			@next_replacement = 'a'
+			
+			if ruletext =~ /rule\s+([\w\_\-]+)(\s*:\s*(\w[\w\s]+\w))?\s*\{\s*(meta:\s*(.*?))?strings:\s*(.*?)\s*condition:\s*(.*?)\s*\}/m
+				name,_,tags,ifmeta,meta,strings,condition = $~.captures
+				@name = name
+				@tags = tags.strip.split(/[,\s]+/) if tags
+				@meta = {}
+				meta.split(/\n/).each do |m|
+					k,v = m.strip.split(/\s*=\s*/,2)
+					if v
+						@meta[k] = v
 					end
-				}.map{ |sp|
-					condition_rearrange2(sp)
-				}
-			else
-				subpart
+				end
+				@normalized_strings = []
+				@strings = strings.split(/\n/).map do |s|
+					# strip off the spaces from the edges and then replace the first = with ' = '.
+					s = s.strip
+					if s[/\s*=\s*/,0]
+						s[/\s*=\s*/,0] = " = "
+					end
+					if s =~ /= \{([0-9a-fA-F\s]+)\}/
+						# normalize the hex string
+						hexstr = $1.gsub(/\s+/,'').downcase.scan(/../).join(" ")
+						s = s.gsub(/= \{([0-9a-fA-F\s]+)\}/, "= { #{hexstr} }")
+					end
+					key, val = s.split(/ = /,2)
+					if val
+						@normalized_strings << val
+					else
+						@normalized_strings << s
+					end
+					s
+				end
+				@normalized_strings.sort!
+				@condition = condition.split(/\n/).map{|x| x.strip}
+				@normalized_condition = @condition.map{|x| _normalize_condition(x)}
 			end
+		end
+		
+		def _normalize_condition(condition)
+			condition.gsub(/[\$\#]\w+/) do |x|
+				key = x[1,1000]
+				if not @lookup_table[key]
+					@lookup_table[key] = @next_replacement
+					@next_replacement = (@next_replacement[0] + 1).chr
+				end
+				x[0].chr+@lookup_table[key]
+			end
+		end
+		
+		def normalize
+			text = "rule #{@name} "
+			if @tags and @tags.length > 0
+				text += ": #{@tags.join(' ')} "
+			end
+			text += "{\n"
+			if @meta and @meta.length > 0
+				text += "  meta:\n"
+				@meta.each do |k,v|
+					text += "    #{k} = #{v}\n"
+				end
+			end
+			if @strings and @strings.length > 0
+				text += "  strings:\n"
+				@strings.each do |s|
+					if s =~ /\w/
+						text += "    #{s}\n"
+					end
+				end
+			end
+			if @condition and @condition.length > 0
+				text += "  condition:\n"
+				@condition.each do |c|
+					if c =~ /\w/
+						text += "    #{c}\n"
+					end
+				end
+			end
+			text + "}"
+		end
+		
+		def hash
+			normalized_strings = @normalized_strings.join("%")
+			normalized_condition = @normalized_condition.join("%")
+			strings_hash = Digest::MD5.hexdigest(normalized_strings)
+			condition_hash = Digest::MD5.hexdigest(normalized_condition)
+			"yn#{VERSION}:#{strings_hash[-16,16]}:#{condition_hash[-10,10]}"
 		end
 	end
 	
-	class Normalizer
-		def initialize
-		end
-		
-		def normalize(rule)
-			raise "Invalid rule: rules must begin with the word 'rule'" unless rule =~ /^\s*rule\s/
-			raise "Invalid rule: rules must end with a closing bracket, }" unless rule =~ /\}\s*$/
-			if rule =~ /^\s*rule\s+(\w+)(\s*:\s*(\w[\w\s]+\w))?\s*\{\s*meta:\s*(.*?)\s*strings:\s*(.*?)\s*condition:\s*(.*?)\s*\}\s*$/m
-				#pp $~.captures
-				name,_,tags,meta,strings,condition = $~.captures
-				tags = tags.split(/\s+/) if tags
-				metatags = {}
-				meta.split(/\n+/).each do |x|
-					a,b = x.split(/\s*=\s*/)
-					metatags[a.strip] = b.strip
-				end
-				strings = strings.split(/\n+/).map{|x| x.strip}
-				condition = condition.strip
-				Rule.new(name,tags,metatags,strings,condition)
-			else
-				nil
+	class Splitter
+		def Splitter.split(ruleset)
+			rules = ruleset.gsub(/[\r\n]+/,"\n").gsub(/^\s*\/\/.*$/,'').scan(/(rule\s+([\w\_\-]+)(\s*:\s*(\w[\w\s]+\w))?\s*\{\s*(meta:\s*(.*?))?strings:\s*(.*?)\s*condition:\s*(.*?)\s*\})/m).map do |rule|
+				YaraRule.new(rule[0])
 			end
 		end
 	end
--- test/test_yara-normalize.rb
+++ test/test_yara-normalize.rb
@@ -1,11 +1,13 @@
 require 'helper'
+require 'pp'
+
 class TestYaraNormalize < Test::Unit::TestCase
 	should "normalize a simple signature" do
 		sig =<<EOS
 rule newIE0daymshtmlExec
 {
 	meta:
-		author = "adnan.shukor@gmail.com"
+		author = "redacted @ gmail.com"
 		ref = "http://blog.vulnhunt.com/index.php/2012/09/17/ie-execcommand-fuction-use-after-free-vulnerability-0day_en/"
 		description = "Internet Explorer CMshtmlEd::Exec() 0day"
 		cve = "CVE-2012-XXXX"
@@ -24,42 +26,27 @@ rule newIE0daymshtmlExec
 		($mshtmlExec_1 and $mshtmlExec_2 and $mshtmlExec_3) or ($mshtmlExec_4 and $mshtmlExec_5 and ($mshtmlExec_6 or $mshtmlExec_7))
 }
 EOS
-		yn = Yara::Normalizer.new
-		nrm = yn.normalize(sig)
-		hash = {}
-		nrm.members.sort.each do |member|
-			hash[member] = nrm[member]
-		end
-		assert_equal({"condition"=>
-		  "($mshtmlExec_1 and $mshtmlExec_2 and $mshtmlExec_3) or ($mshtmlExec_4 and $mshtmlExec_5 and ($mshtmlExec_6 or $mshtmlExec_7))",
-		 "tags"=>nil,
-		 "name"=>"newIE0daymshtmlExec",
-		 "strings"=>
-		  ["$mshtmlExec_1 = /document.execCommand(['\"]selectAll['\"])/ nocase fullword",
-		   "$mshtmlExec_2 = /YMjf\\u0c08\\u0c0cKDogjsiIejengNEkoPDjfiJDIWUAzdfghjAAuUFGGBSIPPPUDFJKSOQJGH/ nocase fullword",
-		   "$mshtmlExec_3 = /<body on(load|select)=['\"]w*?();['\"] on(load|select)=['\"]w*?()['\"]/ nocase",
-		   "$mshtmlExec_4 = /var w{1,} = new Array()/ nocase",
-		   "$mshtmlExec_5 = /window.document.createElement(['\"]img['\"])/ nocase",
-		   "$mshtmlExec_6 = /w{1,}[0][['\"]src['\"]] = ['\"]w{1,}['\"]/ nocase",
-		   "$mshtmlExec_7 = /<iframe src=['\"].*?['\"]/ nocase"],
-		 "meta"=>
-		  {"author"=>"\"adnan.shukor@gmail.com\"",
-		   "description"=>"\"Internet Explorer CMshtmlEd::Exec() 0day\"",
-		   "ref"=>
-		    "\"http://blog.vulnhunt.com/index.php/2012/09/17/ie-execcommand-fuction-use-after-free-vulnerability-0day_en/\"",
-		   "impact"=>"4",
-		   "hide"=>"false",
-		   "cve"=>"\"CVE-2012-XXXX\"",
-		   "version"=>"\"1\""}}, hash)
-		assert_equal("ee2e32d623a0debca271cada22b35b3b904d6abd678cf2a48a87b43cd6302e73f67510c19ffe2f1a", nrm.hash_code)
-	end
-
-	should "normalize a simple signature with tags and spaces instead of tabs" do
+		yn = YaraTools::YaraRule.new(sig)
+		assert_equal("yn01:3c0de1ad64681376:3ff75e9945", yn.hash)
+		assert_equal("newIE0daymshtmlExec", yn.name)
+		assert_equal("\"redacted @ gmail.com\"", yn.meta['author'])
+		assert_equal(["$mshtmlExec_1 = /document.execCommand(['\"]selectAll['\"])/ nocase fullword",
+		 "$mshtmlExec_2 = /YMjf\\u0c08\\u0c0cKDogjsiIejengNEkoPDjfiJDIWUAzdfghjAAuUFGGBSIPPPUDFJKSOQJGH/ nocase fullword",
+		 "$mshtmlExec_3 = /<body on(load|select)=['\"]w*?();['\"] on(load|select)=['\"]w*?()['\"]/ nocase",
+		 "$mshtmlExec_4 = /var w{1,} = new Array()/ nocase",
+		 "$mshtmlExec_5 = /window.document.createElement(['\"]img['\"])/ nocase",
+		 "$mshtmlExec_6 = /w{1,}[0][['\"]src['\"]] = ['\"]w{1,}['\"]/ nocase",
+		 "$mshtmlExec_7 = /<iframe src=['\"].*?['\"]/ nocase"], yn.strings)
+		assert_equal(
+			["($mshtmlExec_1 and $mshtmlExec_2 and $mshtmlExec_3) or ($mshtmlExec_4 and $mshtmlExec_5 and ($mshtmlExec_6 or $mshtmlExec_7))"], 
+			yn.condition
+		)
+		hash1 = yn.hash
 		sig =<<EOS
 rule newIE0daymshtmlExec : tag1 tag2 tag3
 {
   meta:
-    author = "adnan.shukor@gmail.com"
+    author = "redacted @ gmail.com"
     ref = "http://blog.vulnhunt.com/index.php/2012/09/17/ie-execcommand-fuction-use-after-free-vulnerability-0day_en/"
     description = "Internet Explorer CMshtmlEd::Exec() 0day"
     cve = "CVE-2012-XXXX"
@@ -78,66 +65,25 @@ rule newIE0daymshtmlExec : tag1 tag2 tag3
     ($mshtmlExec_1 and $mshtmlExec_2 and $mshtmlExec_3) or ($mshtmlExec_4 and $mshtmlExec_5 and ($mshtmlExec_6 or $mshtmlExec_7))
 }
 EOS
-		yn = Yara::Normalizer.new
-		nrm = yn.normalize(sig)
-		hash = {}
-		nrm.members.sort.each do |member|
-			hash[member] = nrm[member]
-		end
-		assert_equal({"condition"=>
-		  "($mshtmlExec_1 and $mshtmlExec_2 and $mshtmlExec_3) or ($mshtmlExec_4 and $mshtmlExec_5 and ($mshtmlExec_6 or $mshtmlExec_7))",
-		 "tags"=>["tag1","tag2","tag3"],
-		 "name"=>"newIE0daymshtmlExec",
-		 "strings"=>
-		  ["$mshtmlExec_1 = /document.execCommand(['\"]selectAll['\"])/ nocase fullword",
-		   "$mshtmlExec_2 = /YMjf\\u0c08\\u0c0cKDogjsiIejengNEkoPDjfiJDIWUAzdfghjAAuUFGGBSIPPPUDFJKSOQJGH/ nocase fullword",
-		   "$mshtmlExec_3 = /<body on(load|select)=['\"]w*?();['\"] on(load|select)=['\"]w*?()['\"]/ nocase",
-		   "$mshtmlExec_4 = /var w{1,} = new Array()/ nocase",
-		   "$mshtmlExec_5 = /window.document.createElement(['\"]img['\"])/ nocase",
-		   "$mshtmlExec_6 = /w{1,}[0][['\"]src['\"]] = ['\"]w{1,}['\"]/ nocase",
-		   "$mshtmlExec_7 = /<iframe src=['\"].*?['\"]/ nocase"],
-		 "meta"=>
-		  {"author"=>"\"adnan.shukor@gmail.com\"",
-		   "description"=>"\"Internet Explorer CMshtmlEd::Exec() 0day\"",
-		   "ref"=>
-		    "\"http://blog.vulnhunt.com/index.php/2012/09/17/ie-execcommand-fuction-use-after-free-vulnerability-0day_en/\"",
-		   "impact"=>"4",
-		   "hide"=>"false",
-		   "cve"=>"\"CVE-2012-XXXX\"",
-		   "version"=>"\"1\""}}, hash)
-		assert_equal("ee2e32d623a0debca271cada22b35b3b904d6abd678cf2a48a87b43cd6302e73f67510c19ffe2f1a", nrm.hash_code)
-	end
-
-	should "normalize a simple signature that has been rearranged" do
-		sig =<<EOS
-rule newIE0daymshtmlExec
-{
-  meta:
-    author = "adnan.shukor@gmail.com"
-    ref = "http://blog.vulnhunt.com/index.php/2012/09/17/ie-execcommand-fuction-use-after-free-vulnerability-0day_en/"
-    description = "Internet Explorer CMshtmlEd::Exec() 0day"
-    cve = "CVE-2012-XXXX"
-    version = "1"
-    impact = 4
-    hide = false
-  strings:
-    $mshtmlExec_3 = /\<body\son(load|select)=['"]\w*?\(\)\;['"]\son(load|select)=['"]\w*?\(\)['"]/ nocase
-    $mshtmlExec_5 = /window\.document\.createElement\(['"]img['"]\)/ nocase
-    $mshtmlExec_6 = /\w{1,}\[0\]\[['"]src['"]\]\s\=\s['"]\w{1,}['"]/ nocase
-    $mshtmlExec_4 = /var\s\w{1,}\s=\snew\sArray\(\)/ nocase
-    $mshtmlExec_1 = /document\.execCommand\(['"]selectAll['"]\)/ nocase fullword
-    $mshtmlExec_7 = /\<iframe\ssrc=['"].*?['"]/ nocase
-    $mshtmlExec_2 = /YMjf\\u0c08\\u0c0cKDogjsiIejengNEkoPDjfiJDIWUAzdfghjAAuUFGGBSIPPPUDFJKSOQJGH/ nocase fullword
-  condition:
-    ($mshtmlExec_4 and ($mshtmlExec_6 or $mshtmlExec_7) and $mshtmlExec_5) or ($mshtmlExec_1 and $mshtmlExec_2 and $mshtmlExec_3)
-}
-EOS
-		yn = Yara::Normalizer.new
-		nrm = yn.normalize(sig)
-		assert_equal("ee2e32d623a0debca271cada22b35b3b904d6abd678cf2a48a87b43cd6302e73f67510c19ffe2f1a", nrm.hash_code)
+		yn = YaraTools::YaraRule.new(sig)
+		assert_equal(hash1, yn.hash)
+		assert_equal("newIE0daymshtmlExec", yn.name)
+		assert_equal(["tag1","tag2","tag3"], yn.tags)
+		assert_equal("\"redacted @ gmail.com\"", yn.meta['author'])
+		assert_equal(["$mshtmlExec_1 = /document.execCommand(['\"]selectAll['\"])/ nocase fullword",
+		 "$mshtmlExec_2 = /YMjf\\u0c08\\u0c0cKDogjsiIejengNEkoPDjfiJDIWUAzdfghjAAuUFGGBSIPPPUDFJKSOQJGH/ nocase fullword",
+		 "$mshtmlExec_3 = /<body on(load|select)=['\"]w*?();['\"] on(load|select)=['\"]w*?()['\"]/ nocase",
+		 "$mshtmlExec_4 = /var w{1,} = new Array()/ nocase",
+		 "$mshtmlExec_5 = /window.document.createElement(['\"]img['\"])/ nocase",
+		 "$mshtmlExec_6 = /w{1,}[0][['\"]src['\"]] = ['\"]w{1,}['\"]/ nocase",
+		 "$mshtmlExec_7 = /<iframe src=['\"].*?['\"]/ nocase"], yn.strings)
+		assert_equal(
+			["($mshtmlExec_1 and $mshtmlExec_2 and $mshtmlExec_3) or ($mshtmlExec_4 and $mshtmlExec_5 and ($mshtmlExec_6 or $mshtmlExec_7))"], 
+			yn.condition
+		)
 	end
 	
-	should "normalize a simple signature that has been rearranged" do
+	should "normalize a simple signature that has a condition of 'any of them'" do
 		sig =<<EOS
 rule DataConversion__wide : IntegerParsing DataConversion {
 	meta:
@@ -151,22 +97,16 @@ rule DataConversion__wide : IntegerParsing DataConversion {
 		any of them
 }
 EOS
-		yn = Yara::Normalizer.new
-		nrm = yn.normalize(sig)
-		hash = {}
-		nrm.members.sort.each do |member|
-			hash[member] = nrm[member]
-		end
-		assert_equal({"tags"=>["IntegerParsing", "DataConversion"],
-		 "name"=>"DataConversion__wide",
-		 "condition"=>"any of them",
-		 "strings"=>
-		  ["$ = \"wtoi\" nocase",
-		   "$ = \"wtol\" nocase",
-		   "$ = \"wtof\" nocase",
-		   "$ = \"wtodb\" nocase"],
-		 "meta"=>{"weight"=>"1"}},hash)
-		assert_equal("dacfb7f79e2ad96cb66c4784323d91e09e8ad2f8c214c8ea0a52e3a3bda71e6612f02361609e0f7a", nrm.hash_code)
+		yn = YaraTools::YaraRule.new(sig)
+		assert_equal("yn01:488085c947cb22ed:d936fceffe", yn.hash)
+		assert_equal("1", yn.meta['weight'])
+		assert_equal("DataConversion__wide", yn.name)
+		assert_equal(["IntegerParsing", "DataConversion"], yn.tags)
+		assert_equal(["$ = \"wtoi\" nocase",
+		 "$ = \"wtol\" nocase",
+		 "$ = \"wtof\" nocase",
+		 "$ = \"wtodb\" nocase"], yn.strings)
+		assert_equal(["any of them"], yn.condition)
 	end
 end
 
